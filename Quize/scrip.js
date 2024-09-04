a881a841d860cd6ebc62f5d622f35c063a5b6f6a
Usman-52 <usman914.764@gmail.com>
2024-09-04 23:58:12 +0500

    i add javaScript file and code

---
+++ Quize/scrip.js
@@ -0,0 +1,37 @@
+export {};
+
+var questions = [
+    {
+        question: ["What is the purpose of the meta tag in HTML?"],
+        choices: [
+            "To define metadata for the document",
+            "To link to external stylesheets",
+            "To create a header section",
+            "To include JavaScript code"
+        ],
+        correctAnswer: 0
+    },
+
+    {
+        question: ["Which attribute is used to uniquely identify an element in HTML?"],
+        choices: [
+            "Class",
+            "Id",
+            "Name",
+            "Style"
+        ],
+        correctAnswer: 1
+    },
+
+    {
+        question: ["Which property is used to change the background color of an element?"],
+        choices: [
+            "color",
+            "background-color",
+            "bgcolor",
+            "background"
+        ],
+
+        
+    }
+]
